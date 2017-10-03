4975697ba0951c020fbde8941e64fcb236eb96b2
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-10-03 20:54:41 +0200

    Fix missing i18n for location

--- src/main/resources/META-INF/resources/webjars/service/vm/azure/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/vm/azure/nls/fr/messages.js
@@ -3,6 +3,7 @@ define({
 	"service:vm:azure:application" : "Identifiant d'application",
 	"service:vm:azure:key" : "Clé d'application",
 	"service:vm:azure:name" : "Nom",
+	"service:vm:azure:location" : "Emplacement",
 	"service:vm:azure:subscription" : "Souscription",
 	"error" : {
 		"azure-vm" : "VM invalide",
--- src/main/resources/META-INF/resources/webjars/service/vm/azure/nls/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/vm/azure/nls/messages.js
@@ -6,6 +6,7 @@ define({
 		"service:vm:azure:tenant" : "Tenant ID",
 		"service:vm:azure:name" : "Name",
 		"service:vm:azure:subscription" : "Subscription",
+		"service:vm:azure:location" : "Location",
 		"service:vm:azure:resource-group" : "Resource Group",
 		"error" : {
 			"azure-vm" : "Invalid VM",
